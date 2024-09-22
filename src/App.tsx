4671bb642caddb6e6abace1aa09be4b782c547b9
Tudor Popov <supreme.notfound@gmail.com>
2024-09-22 17:33:44 +0300

    made the appropriate changes to start employee page development

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import "./App.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import LoginPageView from "./components/FullLogin/LoginPage/LoginPageView";
-import DashboardPageView from "./components/dashboard/DashboardPageView.tsx";
+import DashboardPageView from "./components/Manager/dashboard/DashboardPageView.tsx";
+import EmployeesPageView from "./components/Manager/Employees/EmployeesPageView.tsx";
 
 function App() {
   return (
     <div className="bg-main">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={
-              localStorage.getItem("isLoggedIn") === "true"?
-                (<Navigate to="/dashboard" />):
-                  (<Navigate to="/login" />)
-
-          } />
+          <Route
+            path="/"
+            element={
+              localStorage.getItem("isLoggedIn") === "true" ? (
+                <Navigate to="/dashboard" />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
           <Route path="/login" element={<LoginPageView />} />
           <Route path="/dashboard" element={<DashboardPageView />} />
+          <Route path="/employees" element={<EmployeesPageView />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Manager/Employees/EmployeesPageView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EmployeesPageView: React.FC = () => {
+  return <div>Loading404</div>;
+};
+
+export default EmployeesPageView;
